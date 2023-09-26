b87f91440d81605baa028cc3f58b4f81bd4c1ed9
MD. EMON MIAH <md.emonmiahisthebest@gmail.com>
2023-09-26 12:32:32 +0600

    Test1

--- lib/ScreenPages/home_screen.dart
+++ lib/ScreenPages/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:another_carousel_pro/another_carousel_pro.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -63,6 +64,29 @@ class HomeScreen extends StatelessWidget {
                   height: 0.5,
                   color: Colors.white70,
                 ),
+              ),
+              const SizedBox(height: 6,),
+              Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: SizedBox(
+                        height: 200.0,
+                        width: 350.0,
+                        child: AnotherCarousel(
+                          images: const [
+                            AssetImage('assets/images/emon.png'),
+                            NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+                            NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+                          ],
+                          dotSize: 4.0,
+                          dotSpacing: 15.0,
+                          // dotColor: Colors.lightGreenAccent,
+                          // indicatorBgPadding: 5.0,
+                          // dotBgColor: Colors.purple.withOpacity(0.5),
+                          borderRadius: true,
+                        )
+                    ),
+                  ),
               ),
             ],
           ),
